9fef58f04345458a10f90ea1d7b3adbc3ed41367
Маршалов Андрій Олександрович <2510505@KMA.EDU>
2025-09-10 13:45:15 +0300

    add project 2

---
+++ Project2/main.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a, n;
+	char c;
+	printf("Enter num and pos of bit (a+n - set or a-n - unset): ");
+	if (scanf("%d%c%d", &a, &c, &n) != 3 || n < 0 || n > 31)
+	{
+		printf("Invalid input data\n");
+		return -1;
+	}
+	int mask = 1;
+	switch (c)
+	{
+	case '+':
+		printf("%d\n", (mask << n) | a);
+		break;
+	case '-':
+		printf("%d\n", ~(mask << n) & a);
+		break;
+	default:
+		printf("Unknown operator\n");
+		break;
+	}
+	return 0;
+}
